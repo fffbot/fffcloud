415a71eeb5bdaa4c78dd512150c390f413d2da37
fffbot <38205055+fffbot@users.noreply.github.com>
2018-12-26 20:19:54 +0100

    Add FFF 273 and 274

--- 1-download.sh
+++ 1-download.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=html
 
-for i in {1..272}; do
+for i in {1..274}; do
   OUTFILE=${BASEDIR}/fff-${i}.html
   if [[ ! -f ${OUTFILE} ]]; then
     curl -o ${OUTFILE} https://factorio.com/blog/post/fff-${i} || echo ${i} failed >> ./download-failures.log
